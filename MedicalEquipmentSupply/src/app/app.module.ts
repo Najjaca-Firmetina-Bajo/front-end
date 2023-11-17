e2905e0774a05207e86651ceb0b446430ab60ac4
somelijer <116906162+somelijer@users.noreply.github.com>
2023-11-17 13:32:12 +0100

    feat: added materials and created register page

--- MedicalEquipmentSupply/src/app/app.module.ts
+++ MedicalEquipmentSupply/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './feature-modules/layout/home/home.component';
 import { LoginComponent } from './infrastructure/auth/login/login.component';
 import { RegistrationComponent } from './infrastructure/auth/registration/registration.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Angular Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -17,9 +28,20 @@ import { RegistrationComponent } from './infrastructure/auth/registration/regist
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatIconModule,
+    MatSelectModule,
+    MatPaginatorModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- MedicalEquipmentSupply/src/app/infrastructure/auth/registration/registration.component.ts
+++ MedicalEquipmentSupply/src/app/infrastructure/auth/registration/registration.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Registration } from '../model/registration.model';
+//import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-registration',
+  selector: 'xp-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+
+  constructor(
+    //private authService: AuthService,
+    private router: Router
+  ) {}
+  
+  registrationForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    role: new FormControl('Author', [Validators.required]),
+  });
+
+
+  
+
+  register(): void {
+  
+    if (true) {
+      const registration: Registration = {
+        username: this.registrationForm.value.username || '',
+        password: this.registrationForm.value.password || '',
+        role: this.registrationForm.value.role || 'Author'
+      };
+  
+      const formData = new FormData();
+
+  
+      if (this.registrationForm.valid) {
+        //this.authService.register(formData).subscribe({
+        //  next: () => {
+        //    this.router.navigate(['home']);
+        //  },
+        //});
+      }
+      else{
+        console.error(this.registrationForm.errors);
+      }
+    } else {
+      console.error('Profile picture is null or undefined.');
+    }
+  }
+  
+  private isFile(value: any): value is File {
+    return value instanceof File;
+  }
+
 
 }
