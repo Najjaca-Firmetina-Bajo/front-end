ae77ca69ddd7a2f7412ab7cb927e69454398b661
Stefaan01 <116906194+Stefaan01@users.noreply.github.com>
2024-01-28 02:09:09 +0100

    feat: QR code scanner

--- MedicalEquipmentSupply/src/app/feature-modules/administration/reservation-review/reservation-review/reservation-review.component.ts
+++ MedicalEquipmentSupply/src/app/feature-modules/administration/reservation-review/reservation-review/reservation-review.component.ts
@@ -6,6 +6,8 @@ import { Appointment } from '../../model/appointment.model';
 import { QRCodeDto } from '../../model/qrcode.model';
 import { QRCodeEquipment } from '../../model/qr-eq.model';
 import { Equipment } from '../../model/equipment.model';
+import jsQR from 'jsqr';
+import { RegistredUser } from '../../model/registred-user.model';
 
 @Component({
   selector: 'app-reservation-review',
@@ -19,8 +21,18 @@ export class ReservationReviewComponent implements OnInit{
   allQRCodes: QRCodeDto[] = []
   allQRCodeEquipment: QRCodeEquipment[] = []
   allEquipment: Equipment[] = []
+  allRegisteredUsers: RegistredUser[] = []
 
   unexpiredAppointmentsForCA: Appointment[] = []
+  qrCodeAppointmentId: string = "";
+
+  reservationDetails: { reservationNumber: number, registeredUser: string, equipments: string[], eids: number[], quantities: number[] } = {
+    reservationNumber: -1,
+    registeredUser: "",
+    equipments: [],
+    eids: [],
+    quantities: []
+  }  
 
   companyAdmins: CompanyAdministrator[] = []
   loggedCA: CompanyAdministrator = {
@@ -52,6 +64,16 @@ export class ReservationReviewComponent implements OnInit{
     this.administrationService.getAllCompanyAdministrators().subscribe({
       next: (result: CompanyAdministrator[]) => {
           this.companyAdmins = result;
+          this.getAllRegisteredUsers();
+      },
+      error: () => { }
+    });
+  }
+
+  getAllRegisteredUsers(): void {
+    this.administrationService.getAllRegisteredUsers().subscribe({
+      next: (result: RegistredUser[]) => {
+          this.allRegisteredUsers = result;
           this.getLoggedUser();
       },
       error: () => { }
@@ -74,6 +96,7 @@ export class ReservationReviewComponent implements OnInit{
         this.caName = "Issuing of equipment - " + ca.name + " " + ca.surname
       }
     })
+    this.getAllUnexpiredAppointments()
   }
 
   getAllUnexpiredAppointments(): void {
@@ -133,6 +156,152 @@ export class ReservationReviewComponent implements OnInit{
       }
     })
   }
+
+  //TODO: what if is not QR code for this CA?
+  findAppointment(): void {
+    let flag = false;
+    this.allExpiredAppointments.forEach(uap => { //this.unexpiredAppointmentsForCA.forEach
+      const qrid = parseInt(this.qrCodeAppointmentId, 10);
+      if(uap.id === qrid) {
+        flag = true;
+        this.reservationDetails.eids.length = 0;
+        this.reservationDetails.equipments.length = 0;
+        this.reservationDetails.registeredUser = "";
+        this.reservationDetails.reservationNumber = -1;
+        this.findQRCodeInformations(uap);
+      }
+    })
+
+    if(!flag) {
+      this.isReservationExpired()
+    }
+  }
+
+  isReservationExpired(): void {
+    let flag = false
+    this.allExpiredAppointments.forEach(a => {
+      const qrid = parseInt(this.qrCodeAppointmentId, 10);
+      if(a.id === qrid) {
+        flag = true;
+        //prikazi poruku da je istekla rezervacija
+      }
+    })
+
+    if(!flag) {
+      this.findAppropriateCompanyAdministrator()
+    }
+  }
+
+  findAppropriateCompanyAdministrator(): void {
+
+  }
+
+  findQRCodeInformations(appointment: Appointment): void {
+    this.allQRCodes.forEach(qr => {
+      if(qr.appointmentId === appointment.id) {
+        this.reservationDetails.reservationNumber = qr.id;
+        this.findRegisteredUser(qr)
+      }
+    })
+  }
+
+  findRegisteredUser(qrCode: QRCodeDto): void {
+    this.allRegisteredUsers.forEach(ru => {
+      if(qrCode.registeredUserId === ru.id) {
+        this.reservationDetails.registeredUser = ru.name + " " + ru.surname;
+        this.findQRCodeEquipment(qrCode);
+      }
+    })
+  }
+
+  findQRCodeEquipment(qrCode: QRCodeDto): void {
+    this.allQRCodeEquipment.forEach(qre => {
+      if(qre.qrCode === qrCode.id) {
+        this.allEquipment.forEach(e => {
+          if(qre.equipmentId === e.id) {
+            this.reservationDetails.eids.push(e.id);
+            this.reservationDetails.equipments.push(e.name + " x" + qre.quantity);
+            this.reservationDetails.quantities.push(qre.quantity);
+          }
+        })
+      }
+    })
+  }
+
+  updateStockStatus(): void {
+    let i = 0;
+    this.reservationDetails.eids.forEach(eid => {
+      this.administrationService.updateStockStatus(eid, this.loggedCA.companyId, this.reservationDetails.quantities[i]).subscribe({
+        next: (result: number) => {
+            i += 1;
+        },
+        error: () => { }
+      });
+    })
+  }
+
+  downloadEquipmentAndSendEmail(): void {
+    const qrid = parseInt(this.qrCodeAppointmentId, 10);
+    this.administrationService.downloadEquipmentAndSendEmail(qrid, this.reservationDetails.reservationNumber).subscribe({
+      next: (result: number) => {
+
+      },
+      error: () => { }
+    });
+  }
+
+  show(): void {
+    console.log(this.reservationDetails)
+  }
+
+  handleFileInput(event: any): void {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        const imageDataUrl = e.target.result;
+        this.processImageData(imageDataUrl);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  processImageData(imageDataUrl: string): void {
+    const img = new Image();
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      const context = canvas.getContext('2d');
+      context?.drawImage(img, 0, 0, img.width, img.height);
+
+      const imageData = context?.getImageData(0, 0, img.width, img.height);
+      if (imageData) {
+        const code = jsQR(imageData.data, img.width, img.height);
+
+        if (code) {
+          console.log('Detected QR Code:', code);
+          this.qrCodeAppointmentId = code.data;
+          console.log(this.qrCodeAppointmentId) //appointment id
+        } else {
+          console.log('QR kod nije pronađen.');
+          this.qrCodeAppointmentId = "";
+        }
+      } else {
+        console.log('Nemoguće dobiti podatke o slici.');
+        this.qrCodeAppointmentId = "";
+      }
+    };
+
+    img.src = imageDataUrl;
+  }
+
+
 
   //sada CA prilaze QRCode. QRCode se trazi u njegovim listama za pocetak, prikazu se sve informacije koje QRCode sadrzi.
   //u zavisnosti od datuma, prikazuje se opcija "issue equipment" + obavjestenje o mejlu
